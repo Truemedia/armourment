f01d94f9fdb893e893b1439181cde8c1bb0bf7e4
Wade Penistone <w.penistone@gmail.com>
2019-05-06 10:04:55 +0100

    CLI chat interface

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,19 @@
 #! /usr/bin/env node
+const readline = require('readline');
+const jsonFile = require('jsonfile');
+const Dialog = require('./dialog');
 const yargs = require('yargs')
     .usage("$0 --e=utterance")
     .option('export', {
       "alias": "e",
       "default": "utterance",
       "describe": "Type of file to export (utterance, speech, timeline)",
+      "type": "string"
+    })
+    .option('killswitch', {
+      "alias": "k",
+      "default": "CUT",
+      "describe": "Keyword used to end conversation",
       "type": "string"
     })
     .option('intent', {
@@ -23,6 +32,7 @@ const yargs = require('yargs')
     .alias('h', 'help');
 
 // let {timeline, export} = yargs.argv;
+
 const Armourment = require('./index');
 
 let armour = new Armourment({
@@ -33,3 +43,27 @@ armour.convo.speak('bot', 'Hello world');
 armour.convo.speak('person', 'Hi');
 
 console.log(armour.convo.export('timeline'));
+
+// CUI
+let rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('CHAT> ');
+rl.prompt();
+
+rl.on('line', (line) => {
+    let content = line.trim();
+    rl.prompt();
+    armour.convo.addDialog( Dialog.fromLine(content) );
+    if (content == yargs.argv.killswitch) {
+      jsonFile.writeFileSync('output.json', armour.convo.timeline);
+      killChat();
+    }
+  })
+  .on('close', function() {
+    killChat();
+  });
+
+function killChat()
+{
+  console.log('Closing session!');
+  process.exit(0);
+}
--- src/dialog.js
+++ src/dialog.js
@@ -12,6 +12,14 @@ class Dialog
     this.msg = txt;
     this.timestamp = new Date;
   }
+
+  static fromLine(ln)
+  {
+    let [speaker, msg] = ln.split(': ');
+    let instance = new this(speaker);
+    instance.txt = msg;
+    return instance;
+  }
 }
 
 module.exports = Dialog;
